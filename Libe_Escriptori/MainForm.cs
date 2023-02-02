7b0619f4db315d6f1a52df7ffc728fc1bd5823f0
PolPuiggros <polpuig1@gmail.com>
2023-02-02 13:10:12 +0100

    Squashed commit of the following:
    commit e6245cf26fa4cb8c5bbc0683efc407c7c0be6bef
    Author: PolPuiggros <polpuig1@gmail.com>
    Date:   Thu Feb 2 13:07:45 2023 +0100
    
        Fixed_Items_Acabat
    
    commit 25d48e0c1ee092fab4640f546e877a0712a7d7bd
    Author: PolPuiggros <ppuiggrosp2122@politecnics.barcelona>
    Date:   Wed Feb 1 12:42:33 2023 +0100
    
        LIBE 26 - Menu Nav
    
        Menú acabat
    
    commit 63bed4100d3821066cb177b25550c39af2698184
    Author: PolPuiggros <ppuiggrosp2122@politecnics.barcelona>
    Date:   Tue Jan 31 13:47:17 2023 +0100
    
        Funcionalitat menu
    
    commit d208ea558f95ad2bc0808fd1a5e94a3101553a6a
    Author: Pol <polpuig1@gmail.com>
    Date:   Mon Jan 30 13:39:51 2023 +0100
    
        Menús sense porgramar

---
+++ Libe_Escriptori/MainForm.cs
@@ -0,0 +1,180 @@
+namespace Libe_Escriptori
+{
+    public partial class MainForm : Form
+    {
+        Button previousButton;
+        private bool mouseDown;
+        private Point lastLocation;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            customDesign();
+            previousButton = buttonInici;
+        }
+        private void customDesign()
+        {
+            selectedEntry(buttonInici);
+            panelUsuaris.Visible = false;
+            panelCentreSubmenu.Visible = false;
+        }
+
+        private void hideSubMenu()
+        {
+            if (panelUsuaris.Visible == true)
+            {
+                panelUsuaris.Visible = false;
+            }     
+
+            if (panelCentreSubmenu.Visible == true)
+            {
+                panelCentreSubmenu.Visible = false;
+            }
+               
+        }
+        private void showSubMenu(Panel submenu)
+        {
+            if (!submenu.Visible)
+            {
+                hideSubMenu();
+                submenu.Visible = true;
+            }
+            else
+            {
+                submenu.Visible = false;
+            }
+        }
+        private void buttonInici_Click(object sender, EventArgs e)
+        {
+            DefaultButton(buttonInici, previousButton, false);
+            selectedEntry(buttonInici);
+            previousButton = buttonInici;
+            hideSubMenu();
+        }
+
+        private void buttonGestionarUsuaris_Click(object sender, EventArgs e)
+        {
+            DefaultButton(buttonGestionarUsuaris, previousButton, false);
+            selectedEntry(buttonGestionarUsuaris);
+            previousButton = buttonGestionarUsuaris;
+            showSubMenu(panelUsuaris);
+        }
+        private void buttonGestionarAlumnes_Click(object sender, EventArgs e)
+        {
+            DefaultButton(buttonGestionarAlumnes, previousButton,true);
+            selectedEntry(buttonGestionarAlumnes);
+            previousButton = buttonGestionarAlumnes;
+        }
+
+        private void buttonGestionarProfessors_Click(object sender, EventArgs e)
+        {
+            DefaultButton(buttonGestionarProfessors, previousButton, true);
+            selectedEntry(buttonGestionarProfessors);
+            previousButton = buttonGestionarProfessors;
+        }
+
+        private void buttonGestionarCursos_Click(object sender, EventArgs e)
+        {
+            DefaultButton(buttonGestionarCursos, previousButton, false);
+            selectedEntry(buttonGestionarCursos);
+            previousButton = buttonGestionarCursos;
+            hideSubMenu();
+        }
+
+        private void buttonGestionarGrups_Click(object sender, EventArgs e)
+        {
+            DefaultButton(buttonGestionarGrups, previousButton, false);
+            selectedEntry(buttonGestionarGrups);
+            previousButton = buttonGestionarGrups;
+            hideSubMenu();
+        }
+
+        private void buttonCentre_Click(object sender, EventArgs e)
+        {
+            DefaultButton(buttonCentre, previousButton, false);
+            selectedEntry(buttonCentre);
+            previousButton = buttonCentre;
+            showSubMenu(panelCentreSubmenu);
+        }
+        private void buttonZonesValidables_Click(object sender, EventArgs e)
+        {
+            DefaultButton(buttonZonesValidables, previousButton, true);
+            selectedEntry(buttonZonesValidables);
+            previousButton = buttonZonesValidables;
+        }
+        private void buttonDepartamentsCentre_Click(object sender, EventArgs e)
+        {
+            DefaultButton(buttonDepartamentsCentre, previousButton, true);
+            selectedEntry(buttonDepartamentsCentre);
+            previousButton = buttonDepartamentsCentre;
+        }
+
+        private void buttonCalendariCentre_Click(object sender, EventArgs e)
+        {
+            DefaultButton(buttonCalendariCentre, previousButton, true);
+            selectedEntry(buttonCalendariCentre);
+            previousButton = buttonCalendariCentre;
+        }
+
+        private void selectedEntry(Button btn)
+        {
+            btn.Font = new Font(btn.Font.FontFamily, 11);
+            btn.BackColor = Color.FromArgb(164,168,241);
+        }
+        private void DefaultButton(Button btnActual, Button previousBtn, bool isChild)
+        {
+            if (btnActual.Text != previousBtn.Text)
+            {
+                if (!String.IsNullOrEmpty(previousBtn.AccessibleName))
+                {
+                    previousButton.Font = new Font(previousButton.Font.FontFamily, 10);
+                    previousButton.BackColor = Color.White;
+                }
+                else
+                {
+                    previousButton.Font = new Font(previousButton.Font.FontFamily, 10);
+                    previousBtn.BackColor = Color.Gainsboro;
+                    
+                }
+            }
+           
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void panelMove_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastLocation = e.Location;
+        }
+
+        private void panelMove_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                this.Location = new Point(
+                    (this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+
+                this.Update();
+            }
+        }
+
+        private void panelMove_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void buttonLogout_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
