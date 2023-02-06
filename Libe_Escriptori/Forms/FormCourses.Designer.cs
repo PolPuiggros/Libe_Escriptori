a06e3bed30aa9c2328d20cdab120d11bfb2c82b3
MarcelHomar <99321235+MarcelHomar@users.noreply.github.com>
2023-02-06 12:27:32 +0100

    Crear directorio

---
+++ Libe_Escriptori/Forms/FormCourses.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Libe_Escriptori.Forms
+{
+    partial class FormCourses
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormCourses
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1005, 650);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormCourses";
+            this.Text = "FormCourses";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
