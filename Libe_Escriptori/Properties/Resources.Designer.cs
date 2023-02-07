c16c18050c7b3a489ecd809953adb607c1704013
PolPuiggros <polpuig1@gmail.com>
2023-02-07 12:15:01 +0100

    Merge Login to main

--- Libe_Escriptori/Properties/Resources.Designer.cs
+++ Libe_Escriptori/Properties/Resources.Designer.cs
@@ -153,6 +153,16 @@ namespace Libe_Escriptori.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap lines_yellow {
+            get {
+                object obj = ResourceManager.GetObject("lines_yellow", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap minus {
             get {
                 object obj = ResourceManager.GetObject("minus", resourceCulture);
