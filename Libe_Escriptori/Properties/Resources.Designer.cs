cb747b3534769cd0f537248e22671e2e1ea71945
MarcelHomar <99321235+MarcelHomar@users.noreply.github.com>
2023-02-08 13:39:32 +0100

    correct

--- Libe_Escriptori/Properties/Resources.Designer.cs
+++ Libe_Escriptori/Properties/Resources.Designer.cs
@@ -171,7 +171,7 @@ namespace Libe_Escriptori.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap minus {
             get {
